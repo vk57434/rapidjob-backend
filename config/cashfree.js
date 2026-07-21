ab8425fb8a7d9ea0ede62db60519ad2aeb0e5fa6
vk57434 <vk4492020@gmail.com>
2026-07-21 20:37:07 +0530

    fixing payment issue

--- config/cashfree.js
+++
@@ -1,9 +0,0 @@
-const cf = require('cashfree-pg');
-
-const Cashfree = new cf.Cashfree();
-
-Cashfree.XClientId = process.env.CASHFREE_CLIENT_ID;
-Cashfree.XClientSecret = process.env.CASHFREE_CLIENT_SECRET;
-Cashfree.XEnvironment = process.env.CASHFREE_ENV === 'PRODUCTION' ? cf.CFEnvironment.PRODUCTION : cf.CFEnvironment.SANDBOX;
-
-module.exports = Cashfree;
